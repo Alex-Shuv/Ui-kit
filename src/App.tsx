5779d72ba02b750f4ced7baec78dfa95fa035f72
Alexander Shuvalov <alex-shuv@mail.ru>
2023-12-28 22:32:53 +0300

    fixtoast

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,99 @@
-import "./App.css";
-import { Tab } from "./components/Tabs/TabItem";
-import { TabsComponent } from "./components/Tabs/Tabs";
-import { ToastProvider } from "./components/Toast/ToastProvider";
-import { ToastRoot } from "./components/Toast/ToastRoot";
+import { useEffect, useRef, useState } from 'react';
+import { Tab } from './components/Tabs/TabItem';
+import { TabsComponent } from './components/Tabs/Tabs';
+import { ToastProvider } from './components/Toast/ToastProvider';
+import { ToastRoot } from './components/Toast/ToastRoot';
+import './App.css';
+import './components/Toast/styles.css';
 
 export const App = () => {
-  const [open, setOpen] = React.useState(false);
-  const eventDateRef = React.useRef(new Date());
-  const timerRef = React.useRef(0);
+	const [open, setOpen] = useState(false);
+	const eventDateRef = useRef(new Date());
+	const timerRef = useRef(0);
 
-  React.useEffect(() => {
-    return () => clearTimeout(timerRef.current);
-  }, []);
-  return (
-    <>
-      {/* <TabsComponent
-        classNameRoot="Tabs"
-        classNameList="TabList"
-        defaultValue="tab1"
-      >
-        <div>
-          <Tab
-            classNameContent="TabContent"
-            classNameTitle="TabTitle"
-            title="Account"
-            value="tab1"
-          >
-            <input className="TabInput" type="text" placeholder="UserName" />
-            <input className="TabInput" type="email" placeholder="Email" />
-          </Tab>
-        </div>
-        <div>
-          <Tab
-            classNameContent="TabContent"
-            classNameTitle="TabTitle"
-            title="Password"
-            value="tab2"
-          >
-            <input
-              className="TabInput"
-              type="password"
-              placeholder="Password"
-            />
-            <input
-              className="TabInput"
-              type="password"
-              placeholder="Repeat New Password"
-            />
-          </Tab>
-        </div>
-      </TabsComponent> */}
-      <ToastRoot isOpen={open}>
-        <ToastProvider titleText="Scheduled: Catch up">
-          <div>
-            <time
-              className="ToastDescription"
-              dateTime={eventDateRef.current.toISOString()}
-            >
-              {prettyDate(eventDateRef.current)}
-            </time>
-          </div>
-          <div>
-            <button className="Button small green">Undo</button>
-          </div>
-        </ToastProvider>
-      </ToastRoot>
-    </>
-  );
+	useEffect(() => {
+		return () => clearTimeout(timerRef.current);
+	}, []);
+	return (
+		<>
+			<TabsComponent
+				classNameRoot='Tabs'
+				classNameList='TabList'
+				defaultValue='tab1'
+			>
+				<div>
+					<Tab
+						classNameContent='TabContent'
+						classNameTitle='TabTitle'
+						title='Account'
+						value='tab1'
+					>
+						<input className='TabInput' type='text' placeholder='UserName' />
+						<input className='TabInput' type='email' placeholder='Email' />
+					</Tab>
+				</div>
+				<div>
+					<Tab
+						classNameContent='TabContent'
+						classNameTitle='TabTitle'
+						title='Password'
+						value='tab2'
+					>
+						<input
+							className='TabInput'
+							type='password'
+							placeholder='Password'
+						/>
+						<input
+							className='TabInput'
+							type='password'
+							placeholder='Repeat New Password'
+						/>
+					</Tab>
+				</div>
+			</TabsComponent>
+			<ToastProvider swipeDirection='right'>
+				<button
+					className='btn'
+					onClick={() => {
+						setOpen(false);
+						window.clearTimeout(timerRef.current);
+						timerRef.current = window.setTimeout(() => {
+							eventDateRef.current = oneWeekAway();
+							setOpen(true);
+						}, 100);
+					}}
+				>
+					Add to calendar
+				</button>
+				<ToastRoot
+					open={open}
+					onOpenChange={setOpen}
+					titleText='Scheduled: Catch up'
+					altText='Goto schedule to undo'
+					descriptionTitle={`${prettyDate(eventDateRef.current)}`}
+					classNameToast='Toast'
+					classNameTitle='ToastTitle'
+					classNameDescription='ToastDescription'
+					classNameAction='ToastButton'
+					classNameViewPort='ToastViewport'
+				>
+					Undo
+				</ToastRoot>
+			</ToastProvider>
+		</>
+	);
 };
+
+function oneWeekAway(date) {
+	const now = new Date();
+	const inOneWeek = now.setDate(now.getDate() + 7);
+	return new Date(inOneWeek);
+}
+
+function prettyDate(date) {
+	return new Intl.DateTimeFormat('en-US', {
+		dateStyle: 'full',
+		timeStyle: 'short',
+	}).format(date);
+}
--- src/components/Toast/ToastProvider.tsx
+++ src/components/Toast/ToastProvider.tsx
@@ -1,43 +1,27 @@
-import * as Toast from "@radix-ui/react-toast";
-import { FC, ReactNode } from "react";
+import * as Toast from '@radix-ui/react-toast';
+import { FC, ReactNode } from 'react';
 
 type ToastProviderProps = {
-  duration?: number;
-  label?: string;
-  swipeDirection: "right" | "left" | "up" | "down";
-  swipeThreshold?: number;
-  children: ReactNode | string;
-  titleText: string;
-  descriptionTitle: ReactNode;
-  altText: string;
-  close?: ReactNode;
-  hotKey?: string[];
+	duration?: number;
+	label?: string;
+	swipeDirection: 'right' | 'left' | 'up' | 'down';
+	children: ReactNode;
+	swipeThreshold?: number;
 };
 
 export const ToastProvider: FC<ToastProviderProps> = ({
-  children,
-  duration,
-  label,
-  swipeDirection,
-  swipeThreshold,
-  titleText,
-  descriptionTitle,
-  altText,
-  close,
-  hotKey,
+	children,
+	duration,
+	label,
+	swipeDirection,
+	swipeThreshold,
 }) => (
-  <Toast.Provider
-    duration={duration}
-    label={label}
-    swipeThreshold={swipeThreshold}
-    swipeDirection={swipeDirection}
-  >
-    <Toast.Root>
-      <Toast.Title>{titleText}</Toast.Title>
-      <Toast.Description>{descriptionTitle}</Toast.Description>
-      <Toast.Action altText={altText}>{children}</Toast.Action>
-      <Toast.Close>{close}</Toast.Close>
-    </Toast.Root>
-    <Toast.Viewport hotkey={hotKey} />
-  </Toast.Provider>
+	<Toast.Provider
+		duration={duration}
+		label={label}
+		swipeThreshold={swipeThreshold}
+		swipeDirection={swipeDirection}
+	>
+		{children}
+	</Toast.Provider>
 );
